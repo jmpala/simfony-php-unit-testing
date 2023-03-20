613414484264f4e1f284f97a80043022d91701ad
Palazzolo, Juan Martin <j.palazzolo@local-brand-x.com>
2023-03-20 16:52:58 +0100

    Chapter 2 WIP 3:40

---
+++ tests/Unit/Entity/DinosaurTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Entity;
+
+use PHPUnit\Framework\TestCase;
+
+class DinosaurTest extends TestCase
+{
+    public function testItWorks(): void
+    {
+        self::assertEquals(1, 1);
+    }
+
+}
